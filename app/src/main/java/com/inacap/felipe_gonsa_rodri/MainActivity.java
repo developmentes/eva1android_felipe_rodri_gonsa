43bde4b18f241ae3f361dd5595d5f7e1e9effc6e
Rodrigo Muñoz <rodrigom2512@gmail.com>
2020-10-07 20:00:35 -0300

    Modificación MainActivity, creación item_harry. Miércoles 7

--- app/src/main/java/com/inacap/felipe_gonsa_rodri/MainActivity.java
+++ app/src/main/java/com/inacap/felipe_gonsa_rodri/MainActivity.java
@@ -1,7 +1,5 @@
 package com.inacap.felipe_gonsa_rodri;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -11,14 +9,11 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import adapter.HarryAdapter;
-import model.HarryPotter;
+import com.inacap.felipe_gonsa_rodri.model.HarryPotter;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -27,35 +22,23 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String URL = "http://hp-api.herokuapp.com/api/characters";
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         processHttp();
-
     }
-// metodo para utilizar el protocolo http
+
     public void processHttp() {
         AsyncHttpClient client = new AsyncHttpClient();
 
         client.get(URL, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String res = new String(responseBody);
+                Log.d("INFO", res);
 
-               try {
-                   String res = new String(responseBody);
-                   Log.d("INFO", res);
-
-
-                   processJson(res);
-
-
-
-               } catch (JSONException e) {
-                   e.printStackTrace();
-               }
-
+                processJson(res);
             }
 
             @Override
@@ -66,50 +49,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void processJson (String res) throws JSONException {
-
-//            JSONObject root = new JSONObject(res); //se comenta porque la api se compone de un array en su totalidad
+    public void processJson (String res){
+        try {
             JSONArray harry = new JSONArray(res);
-
-
-            List<HarryPotter> lista = new ArrayList<HarryPotter>();
-
-            for (int i = 0; i <= harry.length(); i++) {
-
-                JSONObject harryObj  = harry.getJSONObject(i);
-
-
-                String name = harryObj.getString("name");
-                String species = harryObj.getString("species");
-                String gender = harryObj.getString("gender");
-                String house = harryObj.getString("house");
-                String dateOfBirth = harryObj.getString("dateOfBirth");
-                int yearOfBirth = harryObj.getInt("yearOfBirth");
-                String image = harryObj.getString("image");
+            List<HarryPotter> list = new ArrayList<>();
+            for (int i = 0; i < harry.length(); i++) {
+                JSONObject harryObj = harry.getJSONObject(i);
+                    String name = harryObj.getString("name");
+                    String species = harryObj.getString("species");
+                    String gender = harryObj.getString("gender");
+                    String house = harryObj.getString("house");
+                    String dateOfBird = harryObj.getString("dateOfBird");
+                    String yearOfBird = harryObj.getString("yearOfBird");
                 JSONObject wand = harryObj.getJSONObject("wand");
+                    String wood = wand.getString("wood");
+                    String core = wand.getString("core");
 
-                String wood = wand.getString("wood");
-                String core = wand.getString("core");
-//creamos un objeto de la clase HarryPotter
-                HarryPotter harryPotter = new HarryPotter(name, species, gender, house, dateOfBirth, yearOfBirth, wood, core, image);
+                HarryPotter harryPotter = new HarryPotter(name, species, gender, house, dateOfBird, yearOfBird, wood, core);
                 Log.d("INFO", harryPotter.getCore() + " " + harryPotter.getDateOfBird() + " " + harryPotter.getGender() + " " + harryPotter.getHouse() + " " +
-                        harryPotter.getName() + " " + harryPotter.getSpecies() + " " + harryPotter.getYearOfBirth() + " " + harryPotter.getWood() + " " + harryPotter.getImage());
-                lista.add(harryPotter);
+                        harryPotter.getName() + " " + harryPotter.getSpecies() + " " + harryPotter.getYearsOfBird() + " " + harryPotter.getWood());
+                list.add(harryPotter);
             }
 
+//            RecyclerView rc = findViewById(R.id.rc_user);
+//
+//            LinearLayoutManager lm = new LinearLayoutManager(this);
+//            lm.setOrientation(RecyclerView.VERTICAL);
+//
+//            UserAdapter adapter = new UserAdapter(this,list,R.layout.item_user);
+//
+//            rc.setLayoutManager(lm);
+//            rc.setAdapter(adapter);
 
-        RecyclerView rc = findViewById(R.id.rc_harry);
 
-        LinearLayoutManager lm = new LinearLayoutManager(this);
-        lm.setOrientation(RecyclerView.VERTICAL);
+        } catch (Exception e) {
 
-        HarryAdapter adapter = new HarryAdapter(this, lista, R.layout.harry_main2);
-
-        rc.setLayoutManager(lm);
-        rc.setAdapter(adapter);
+        }
     }
-
-
 
 
     public void entrar(View view) {
