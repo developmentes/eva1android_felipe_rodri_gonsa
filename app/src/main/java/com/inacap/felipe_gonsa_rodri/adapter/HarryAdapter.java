4b544aa81f35731df1bf1196662e6461c20ee366
developmentes <pipedev1978@gmail.com>
2020-10-10 21:23:33 -0300

    tratando de ver la interfaz

--- app/src/main/java/com/inacap/felipe_gonsa_rodri/adapter/HarryAdapter.java
+++ app/src/main/java/com/inacap/felipe_gonsa_rodri/adapter/HarryAdapter.java
@@ -1,11 +1,10 @@
-package adapter;
+package com.inacap.felipe_gonsa_rodri.adapter;
 
 import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -17,7 +16,9 @@ import com.inacap.felipe_gonsa_rodri.R;
 
 import java.util.List;
 
-import model.HarryPotter;
+import com.inacap.felipe_gonsa_rodri.model.HarryPotter;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class HarryAdapter extends RecyclerView.Adapter<HarryAdapter.HarryHolder> {
 
@@ -46,11 +47,13 @@ public class HarryAdapter extends RecyclerView.Adapter<HarryAdapter.HarryHolder>
     public void onBindViewHolder(@NonNull HarryHolder holder, int position) {
 
         HarryPotter harry = list.get(position);
-            holder.nameHarry.setText(harry.getName());
-            holder.speciesHarry.setText(harry.getSpecies());
-            holder.genderHarry.setText(harry.getGender());
-            holder.url_image = harry.getImage();
-            Glide.with(activity).load(harry.getImage()).into(holder.harryCircleImg);
+
+         holder.nameHarry.setText(harry.name);
+            holder.speciesHarry.setText(harry.species);
+            holder.genderHarry.setText(harry.gender);
+            Glide.with(activity).load(harry.image).into(holder.harryCircleImg);
+        holder.url_image = harry.image;
+
     }
 
     @Override
@@ -59,35 +62,26 @@ public class HarryAdapter extends RecyclerView.Adapter<HarryAdapter.HarryHolder>
     }
 
 
-    public class HarryHolder extends RecyclerView.ViewHolder {
+    public static class HarryHolder extends RecyclerView.ViewHolder {
 
-       ImageView harryCircleImg;
+       CircleImageView harryCircleImg;
        TextView nameHarry;
        TextView speciesHarry;
        TextView genderHarry;
-       RelativeLayout item_card;
-       Button botonHarry;
 
-//       TextView nameHarry2;
-//       TextView speciesHarry2;
-//       TextView genderHarry2;
-
+        RelativeLayout item_card;
         String url_image;
 
         public HarryHolder(@NonNull View itemView) {
             super(itemView);
 
             harryCircleImg = itemView.findViewById(R.id.harrycircleimg);
-            nameHarry      = itemView.findViewById(R.id.nameharry);
+            nameHarry    = itemView.findViewById(R.id.nameharry);
             speciesHarry   = itemView.findViewById(R.id.speciesharry);
             genderHarry    = itemView.findViewById(R.id.genderharry);
+
             item_card      = itemView.findViewById(R.id.item_card);
-//            fondo_Relative      = itemView.findViewById(R.id.fondo_relative);
-//            background_Relative      = itemView.findViewById(R.id.background_relative);
-            botonHarry      = itemView.findViewById(R.id.botonharry);
-//            nameHarry2     = itemView.findViewById(R.id.nameharry2);
-//            speciesHarry2  = itemView.findViewById(R.id.speciesharry2);
-//            genderHarry2   = itemView.findViewById(R.id.genderharry2);
+
 
 
         }//end constructor
--- app/src/main/java/com/inacap/felipe_gonsa_rodri/model/HarryPotter.java
+++ app/src/main/java/com/inacap/felipe_gonsa_rodri/model/HarryPotter.java
@@ -1,99 +1,82 @@
 package com.inacap.felipe_gonsa_rodri.model;
 
 public class HarryPotter {
-    private String name;
-    private String species;
-    private  String gender;
-    private String house;
-    private String dateOfBirth;
-    private int yearOfBirth;
-    private String wood;
-    private  String core;
-    private String image;
-    public HarryPotter(String name, String species, String gender, String house, String dateOfBirth, int yearOfBirth, String wood, String core, String image) {
+
+
+
+    public String name;
+    public String species;
+    public  String gender;
+    public String image;
+
+
+    //
+//    private String house;
+//    private String dateOfBirth;
+//    private int yearOfBirth;
+//    private String wood;
+//    private  String core;
+    public HarryPotter(){
+
+    }
+
+    public HarryPotter(String name, String species, String gender, String image) {
         this.name = name;
         this.species = species;
         this.gender = gender;
-        this.house = house;
-        this.dateOfBirth = dateOfBirth;
-        this.yearOfBirth = yearOfBirth;
-        this.wood = wood;
-        this.core = core;
         this.image = image;
+//
+//        this.house = house;
+//        this.dateOfBirth = dateOfBirth;
+//        this.yearOfBirth = yearOfBirth;
+//        this.wood = wood;
+//        this.core = core;
+
     }
 
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+//
+//    public String getHouse() {
+//        return house;
+//    }
+//
+//    public void setHouse(String house) {
+//        this.house = house;
+//    }
+//
+//    public String getDateOfBird() {
+//        return dateOfBirth;
+//    }
+//
+//    public void setDateOfBird(String dateOfBird) {
+//        dateOfBirth = dateOfBird;
+//    }
+//
+//    public int getYearOfBirth() {
+//        return yearOfBirth;
+//    }
+//
+//    public void setYearOfBirth(int yearOfBirth) {
+//        this.yearOfBirth = yearOfBirth;
+//    }
+//
+//    public String getWood() {
+//        return wood;
+//    }
+//
+//    public void setWood(String wood) {
+//        this.wood = wood;
+//    }
+//
+//    public String getCore() {
+//        return core;
+//    }
+//
+//    public void setCore(String core) {
+//        this.core = core;
+//    }
 
-    public String getSpecies() {
-        return species;
-    }
-
-    public void setSpecies(String species) {
-        this.species = species;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getHouse() {
-        return house;
-    }
-
-    public void setHouse(String house) {
-        this.house = house;
-    }
-
-    public String getDateOfBird() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBird(String dateOfBird) {
-        dateOfBirth = dateOfBird;
-    }
-
-    public int getYearOfBirth() {
-        return yearOfBirth;
-    }
-
-    public void setYearOfBirth(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
-    }
-
-    public String getWood() {
-        return wood;
-    }
-
-    public void setWood(String wood) {
-        this.wood = wood;
-    }
-
-    public String getCore() {
-        return core;
-    }
-
-    public void setCore(String core) {
-        this.core = core;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
 
 
 
